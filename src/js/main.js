21c78811ae14658272be24fead788c24e0367643
whatasoda <a16dc107@dhw.ac.jp>
2017-09-22 17:58:55 +0900

    [add] シード値のルールの追加 ページ更新前と更新後のシード値が同一の場合シード値を更新 右クリックで色のみ更新 左クリックで色を更新し、経過フレーム数をリセットする。

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,8 @@
 
   const seedPattern = /^#[0-9a-vA-V]{12}$/
   let u, v
-  if (seedPattern.test(window.location.hash)) {
+  const postSeed = window.sessionStorage.getItem(['postSeed'])
+  if (seedPattern.test(window.location.hash) && window.location.hash.slice(1) !== postSeed) {
     u = parseInt(window.location.hash.slice(1, 7), 32)
     v = parseInt(window.location.hash.slice(7), 32)
   }
@@ -36,6 +37,20 @@
     ).setCurrent()
     window.location.hash = DHFT2017.seed
   }
+
+  container.addEventListener('click', (e) => {
+    DHFT2017.Particle.reset(true)
+    window.getSelection().removeAllRanges()
+    e.preventDefault()
+  })
+  container.addEventListener('contextmenu', (e) => {
+    DHFT2017.Starter.restart()
+    window.getSelection().removeAllRanges()
+    e.preventDefault()
+  })
+  window.addEventListener('beforeunload', (e) => {
+    window.sessionStorage.setItem(['postSeed'],[DHFT2017.seed])
+  })
 
   DHFT2017.speed = 35
   DHFT2017.Starter.animate()
