4a5ef0ccaf3632069e582323e63b50443035a8b8
taku_matsuzaki <m.takuya19961116@gmail.com>
2017-10-04 01:19:06 +0900

    [Add] gulpfile.jsをエラーに対応させました
    PlumberとNotifyを追加。

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,33 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const sassGlob = require("gulp-sass-glob");
 const csscomb = require('gulp-csscomb');
+const notifier = require('node-notifier');
+const plumber = require('gulp-plumber');
+
+const errorHandler = function(error) {
+    notifier.notify({
+      title: 'コンパイル失敗',
+      message: 'Sassのコンパイルでエラーが発生しました。',
+      sound: 'Glass'
+    }, function () {
+      console.log(error.message);
+    });
+  };
+
+const success = function(success) {
+  notifier.notify({
+    title: 'コンパイル完了',
+    message: 'Sassをコンパイルしました。',
+    sound: 'Funk'
+  }, function () {
+    console.log(success.message);
+  });
+};
 
 gulp.task("sass", function() {
   return gulp.src("./sass/style.scss")
     .pipe(sassGlob())
+    .pipe(plumber({errorHandler: errorHandler}))
     .pipe(sass())
     .pipe(gulp.dest("./css"));
 });
